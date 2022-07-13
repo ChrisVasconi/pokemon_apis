56b8ccfe036ce60d2d4055d7ec5d2c0ad7d740f1
Chris Vasconi <vasconichris@gmail.com>
2022-07-13 15:24:24 -0400

    //THis second one is Pokemon API w/ AXIOS

--- src/components/fetch.jsx
+++ src/components/fetch.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import FetchDisplay from "./FetchDisplay";
+import axios from "axios"
 
 const Fetch = () => {
     const [pokemon, setPokemon] = useState([])
@@ -15,10 +16,17 @@ const Fetch = () => {
             .catch(err => console.log(err))
     }
 
+    const fetchPokemonWithAxios = () => {
+        axios.get(`https://pokeapi.co/api/v2/pokemon?limit=800&offset=0`)
+            .then(response => setPokemon(response.data.results))
+            .catch(err => console.log(err))
+    }
+
     return (
 
         <fieldset>
             <button onClick={fetchPokemon}> Fetch Pokemon </button>
+            <button onClick={fetchPokemonWithAxios}> Fetch Pokemon w/ Axios </button>
 
             { //map function to grab an array, p is for pokemon, i is for the individual pieces
                 pokemon.map((p, i) => {
@@ -27,6 +35,15 @@ const Fetch = () => {
                     )
                 })
             }
+            { //map function to grab an array, p is for pokemon, i is for the individual pieces
+                pokemon.map((p, i) => {
+                    return (
+                        <h3 key={i}>{p.name}</h3>
+                    )
+                })
+            }
+
+
 
         </fieldset>
     )
